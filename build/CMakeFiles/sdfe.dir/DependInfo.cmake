6131aa5fe8160ea0ff1514b8f9939613789d3cad
Two Faced Janus <blcklptn@icloud.com>
2025-01-07 14:13:53 +0500

    save

---
+++ build/CMakeFiles/sdfe.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/twofacedjanus/Documents/SDF-Painter/include/imgui/imgui.cpp" "CMakeFiles/sdfe.dir/include/imgui/imgui.cpp.o" "gcc" "CMakeFiles/sdfe.dir/include/imgui/imgui.cpp.o.d"
+  "/Users/twofacedjanus/Documents/SDF-Painter/include/imgui/imgui_demo.cpp" "CMakeFiles/sdfe.dir/include/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/sdfe.dir/include/imgui/imgui_demo.cpp.o.d"
+  "/Users/twofacedjanus/Documents/SDF-Painter/include/imgui/imgui_draw.cpp" "CMakeFiles/sdfe.dir/include/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/sdfe.dir/include/imgui/imgui_draw.cpp.o.d"
+  "/Users/twofacedjanus/Documents/SDF-Painter/include/imgui/imgui_impl_glfw.cpp" "CMakeFiles/sdfe.dir/include/imgui/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/sdfe.dir/include/imgui/imgui_impl_glfw.cpp.o.d"
+  "/Users/twofacedjanus/Documents/SDF-Painter/include/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/sdfe.dir/include/imgui/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/sdfe.dir/include/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/Users/twofacedjanus/Documents/SDF-Painter/include/imgui/imgui_tables.cpp" "CMakeFiles/sdfe.dir/include/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/sdfe.dir/include/imgui/imgui_tables.cpp.o.d"
+  "/Users/twofacedjanus/Documents/SDF-Painter/include/imgui/imgui_widgets.cpp" "CMakeFiles/sdfe.dir/include/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/sdfe.dir/include/imgui/imgui_widgets.cpp.o.d"
+  "/Users/twofacedjanus/Documents/SDF-Painter/src/callbacks.cc" "CMakeFiles/sdfe.dir/src/callbacks.cc.o" "gcc" "CMakeFiles/sdfe.dir/src/callbacks.cc.o.d"
+  "/Users/twofacedjanus/Documents/SDF-Painter/src/main.cc" "CMakeFiles/sdfe.dir/src/main.cc.o" "gcc" "CMakeFiles/sdfe.dir/src/main.cc.o.d"
+  "/Users/twofacedjanus/Documents/SDF-Painter/src/quad_fs.cc" "CMakeFiles/sdfe.dir/src/quad_fs.cc.o" "gcc" "CMakeFiles/sdfe.dir/src/quad_fs.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sdfe.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdfe.dir/include/imgui/imgui.cpp.o"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui.cpp.o.d"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/sdfe.dir/include/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/sdfe.dir/src/callbacks.cc.o"
+  "CMakeFiles/sdfe.dir/src/callbacks.cc.o.d"
+  "CMakeFiles/sdfe.dir/src/main.cc.o"
+  "CMakeFiles/sdfe.dir/src/main.cc.o.d"
+  "CMakeFiles/sdfe.dir/src/quad_fs.cc.o"
+  "CMakeFiles/sdfe.dir/src/quad_fs.cc.o.d"
+  "sdfe"
+  "sdfe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdfe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
